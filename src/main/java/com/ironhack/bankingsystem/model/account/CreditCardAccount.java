6086854336002bedad8a987fe5bbb366cccf7b6c
Alejandro <amnaredo@gmail.com>
2021-02-06 12:59:10 +0100

    Added types validation on constructors

--- src/main/java/com/ironhack/bankingsystem/model/account/CreditCardAccount.java
+++ src/main/java/com/ironhack/bankingsystem/model/account/CreditCardAccount.java
@@ -2,6 +2,9 @@ package com.ironhack.bankingsystem.model.account;
 
 import com.ironhack.bankingsystem.model.user.AccountHolder;
 
+import javax.validation.constraints.DecimalMax;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.Min;
 import java.math.BigDecimal;
 
 public class CreditCardAccount extends Account {
@@ -15,13 +18,17 @@ public class CreditCardAccount extends Account {
 //    An interestRate
 //    A penaltyFee
 
-    private static final Money DEFAULT_CREDIT_LIMIT = new Money(BigDecimal.valueOf(100L));
-    private static final BigDecimal DEFAULT_INTEREST_RATE = BigDecimal.valueOf(0.2);
+    private static final Double VALID_MIN_CREDIT_LIMIT = 100.0;
+    private static final Double VALID_MAX_CREDIT_LIMIT = 100000.0;
+    private static final Double VALID_MIN_INTEREST_RATE = 0.1;
+    private static final Double VALID_MAX_INTEREST_RATE = 0.2;
+
+    private static final Money DEFAULT_CREDIT_LIMIT = new Money(BigDecimal.valueOf(VALID_MIN_CREDIT_LIMIT));
+    private static final BigDecimal DEFAULT_INTEREST_RATE = BigDecimal.valueOf(VALID_MAX_INTEREST_RATE);
 
 
     private Money creditLimit;
     private BigDecimal interestRate;
-
 
 
     public CreditCardAccount() {
@@ -29,28 +36,23 @@ public class CreditCardAccount extends Account {
 
     public CreditCardAccount(Money balance, AccountHolder primaryOwner, AccountHolder secondaryOwner) {
         super(balance, primaryOwner, secondaryOwner);
-        this.creditLimit = DEFAULT_CREDIT_LIMIT;    //CreditCard accounts have a default creditLimit of 100
+        this.creditLimit = DEFAULT_CREDIT_LIMIT;  // CreditCard accounts have a default creditLimit of 100
         this.interestRate = DEFAULT_INTEREST_RATE;// CreditCards have a default interestRate of 0.2
     }
 
     // CreditCards may be instantiated with a creditLimit higher than 100 but not higher than 100000
-    public CreditCardAccount(Money balance, AccountHolder primaryOwner, AccountHolder secondaryOwner, Money creditLimit) {
-        super(balance, primaryOwner, secondaryOwner);
-        this.creditLimit = creditLimit;
-    }
-
     // CreditCards may be instantiated with an interestRate less than 0.2 but not lower than 0.1
-    public CreditCardAccount(Money balance, AccountHolder primaryOwner, AccountHolder secondaryOwner, BigDecimal interestRate) {
+    public CreditCardAccount(Money balance, AccountHolder primaryOwner, AccountHolder secondaryOwner,
+                             @DecimalMin(value = "100.0", message = "Credit limit must be greater than 100")
+                             @DecimalMax(value = "100000.0", message = "Credit limit must be lesser than 100000")
+                             Double creditLimit,
+                             @DecimalMin(value = "0.1", message = "Interest rate must be greater than 0.1")
+                             @DecimalMax(value = "0.2", message = "Interest rate must be lesser than 0.2")
+                             Double interestRate) {
         super(balance, primaryOwner, secondaryOwner);
-        this.interestRate = interestRate;
+        this.creditLimit = new Money(BigDecimal.valueOf(creditLimit));
+        this.interestRate = BigDecimal.valueOf(interestRate);
     }
-
-    public CreditCardAccount(Money balance, AccountHolder primaryOwner, AccountHolder secondaryOwner, Money creditLimit, BigDecimal interestRate) {
-        super(balance, primaryOwner, secondaryOwner);
-        this.creditLimit = creditLimit;
-        this.interestRate = interestRate;
-    }
-
 
 
     public Money getCreditLimit() {
--- src/main/java/com/ironhack/bankingsystem/model/account/SavingsAccount.java
+++ src/main/java/com/ironhack/bankingsystem/model/account/SavingsAccount.java
@@ -3,6 +3,8 @@ package com.ironhack.bankingsystem.model.account;
 import com.ironhack.bankingsystem.model.account.enums.Status;
 import com.ironhack.bankingsystem.model.user.AccountHolder;
 
+import javax.validation.constraints.DecimalMax;
+import javax.validation.constraints.DecimalMin;
 import java.math.BigDecimal;
 
 public class SavingsAccount extends CheckingAccount {
@@ -22,15 +24,40 @@ public class SavingsAccount extends CheckingAccount {
 //    Do NOT have a monthlyMaintenanceFee
 //    Do have an interestRate
 
+
+
+    private static final Double VALID_MIN_INTEREST_RATE = 0.;
+    private static final Double VALID_MAX_INTEREST_RATE = 0.5;
+    private static final Double VALID_MIN_MINIMUM_BALANCE = 100.;
+    private static final Double VALID_MAX_MINIMUM_BALANCE = 1000.;
+
+    private static final BigDecimal DEFAULT_INTEREST_RATE = BigDecimal.valueOf(0.0025);
+    private static final Money DEFAULT_MINIMUM_BALANCE = new Money(BigDecimal.valueOf(VALID_MAX_MINIMUM_BALANCE));
+
+
     private BigDecimal interestRate;
 
     public SavingsAccount() {
     }
 
-    public SavingsAccount(Money balance, AccountHolder primaryOwner, AccountHolder secondaryOwner, Integer secretKey, BigDecimal interestRate) {
+    public SavingsAccount(Money balance, AccountHolder primaryOwner, AccountHolder secondaryOwner, Integer secretKey) {
         super(balance, primaryOwner, secondaryOwner, secretKey);
         setMonthlyMaintenanceFee(new Money(BigDecimal.ZERO));
-        this.interestRate = interestRate;
+        setMinimumBalance(DEFAULT_MINIMUM_BALANCE);
+        this.interestRate = DEFAULT_INTEREST_RATE;
+    }
+
+    public SavingsAccount(Money balance, AccountHolder primaryOwner, AccountHolder secondaryOwner, Integer secretKey,
+                          @DecimalMin(value = "0.0", message = "Interest rate must be non negative")
+                          @DecimalMax(value = "0.5", message = "Interest rate must be lesser than 0.5")
+                          Double interestRate,
+                          @DecimalMin(value = "100", message = "Minimum balance must be greater than 100")
+                          @DecimalMax(value = "1000", message = "Minimum balance must be lesser than 1000")
+                          Double minimumBalance) {
+        super(balance, primaryOwner, secondaryOwner, secretKey);
+        setMonthlyMaintenanceFee(new Money(BigDecimal.ZERO));
+        setMinimumBalance(new Money(BigDecimal.valueOf(minimumBalance)));
+        this.interestRate = BigDecimal.valueOf(interestRate);
     }
 
 
