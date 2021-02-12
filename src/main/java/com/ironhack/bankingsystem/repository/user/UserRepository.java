aede29d71977e696e0527c485ee66502d54ef805
Alejandro <amnaredo@gmail.com>
2021-02-12 10:18:23 +0100

    Added security layer classes

---
+++ src/main/java/com/ironhack/bankingsystem/repository/user/UserRepository.java
@@ -0,0 +1,14 @@
+package com.ironhack.bankingsystem.repository.user;
+
+import com.ironhack.bankingsystem.model.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
+
+}
